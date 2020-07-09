2141d89810a5cf156b7deeb5d4cb969fcbed4b44
Horki <ivanzvonimirhorvat@gmail.com>
2020-07-09 17:12:36 +0200

    two trees

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+pub mod rbtree;
+pub mod bst;
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn its_42() {
+        assert_eq!(20 + 22, 42);
     }
 }
