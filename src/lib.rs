623ad4f20f932c21d5a51ea21a2ee493d49d6c33
Horki <ivanzvonimirhorvat@gmail.com>
2020-07-24 00:06:54 +0200

    lib: add traversals in trait

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,67 @@ pub trait SedgewickMap<K: Ord, V> {
     fn contains(&self, key: &K) -> bool;
     fn min(&self) -> Option<&K>;
     fn max(&self) -> Option<&K>;
-    fn traverse(&self, traverse: &Traversals) -> std::vec::IntoIter<(&K, &V)>;
+
+    /// A immutable recursive traversals over Binary Trees.
+    ///
+    /// `Pre order`
+    /// `In order`
+    /// `Post order`
+    /// `Level order`
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use treers::bst::BST;
+    /// use treers::{SedgewickMap, Traversals};
+    ///
+    /// let mut bst: BST<char, i32> = BST::new();
+    /// bst.put('c', 3);
+    /// bst.put('d', 4);
+    /// bst.put('b', 2);
+    /// bst.put('a', 1);
+    ///
+    /// // Pre order Traversal by keys
+    /// for (a, _) in bst.traverse(&Traversals::PreOrder) {
+    ///     print!("{}, ", *a);
+    /// }
+    ///
+    /// // In order Traversal by keys
+    /// for (a, _) in bst.traverse(&Traversals::InOrder) {
+    ///     print!("{}, ", *a);
+    /// }
+    ///
+    /// // Post order Traversal by keys
+    /// for (a, _) in bst.traverse(&Traversals::PostOrder) {
+    ///     print!("{}, ", *a);
+    /// }
+    ///
+    /// // Level order Traversal by keys
+    /// for (a, _) in bst.traverse(&Traversals::LevelOrder) {
+    ///     print!("{}, ", *a);
+    /// }
+    /// ```
+    fn traverse(&self, traverse: &Traversals) -> std::vec::IntoIter<(&K, &V)> {
+        let mut vec = Vec::with_capacity(self.size());
+        match traverse {
+            Traversals::PreOrder => self.pre_order(&mut vec),
+            Traversals::InOrder => self.in_order(&mut vec),
+            Traversals::PostOrder => self.post_order(&mut vec),
+            Traversals::LevelOrder => {
+                for level in 1..=self.height() {
+                    self.level_order(&mut vec, level);
+                }
+            }
+        }
+        vec.into_iter()
+    }
+    // TODO: figure out how to make this as a different trait!
+    fn pre_order<'a>(&'a self, vec: &mut Vec<(&'a K, &'a V)>);
+    fn in_order<'a>(&'a self, vec: &mut Vec<(&'a K, &'a V)>);
+    fn post_order<'a>(&'a self, vec: &mut Vec<(&'a K, &'a V)>);
+    fn level_order<'a>(&'a self, vec: &mut Vec<(&'a K, &'a V)>, level: usize);
 }
 
 pub enum Traversals {
