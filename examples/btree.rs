fdc280bd866cda959558d95b33d950584b45ef76
Horki <ivanzvonimirhorvat@gmail.com>
2020-07-30 01:10:58 +0200

    btree: example sample

--- examples/btree.rs
+++ examples/btree.rs
@@ -3,9 +3,12 @@ use treers::SedgewickMap;
 
 fn main() {
     let mut btree = BalancedTree::new();
-    btree.put(1, 4);
+    btree.put(4, 5);
     btree.put(2, 1);
     btree.put(3, 3);
-    btree.put(4, 5);
+    btree.put(1, 4);
+    println!("min(1) = {}", btree.min().unwrap());
+    println!("min(4) = {}", btree.max().unwrap());
+    println!("btree[{}] = {}", 1, btree.get(&1).unwrap());
     dbg!(btree);
 }
