ea4a0a98fc250af8d112c054bb1fd7caa0c4733b
Horki <ivanzvonimirhorvat@gmail.com>
2020-08-04 18:29:29 +0200

    benches: small updated on naming convetions

--- benches/maps.rs
+++ benches/maps.rs
@@ -9,7 +9,7 @@ use treers::rbtree::RedBlackTree;
 use treers::SedgewickMap;
 
 #[bench]
-fn bst_add_one_thousand_asc(b: &mut Bencher) {
+fn bst_add_one_thousand_left_rotate(b: &mut Bencher) {
     let mut bst = BST::new();
     b.iter(|| {
         for i in 1..=1_000_u64 {
@@ -19,7 +19,7 @@ fn bst_add_one_thousand_asc(b: &mut Bencher) {
 }
 
 #[bench]
-fn bst_add_one_thousand_desc(b: &mut Bencher) {
+fn bst_add_one_thousand_right_rotate(b: &mut Bencher) {
     let mut bst = BST::new();
     b.iter(|| {
         for i in (1..=1_000_u64).rev() {
@@ -29,7 +29,7 @@ fn bst_add_one_thousand_desc(b: &mut Bencher) {
 }
 
 #[bench]
-fn std_btree_add_one_thousand(b: &mut Bencher) {
+fn std_btree_add_one_thousand_left_rotate(b: &mut Bencher) {
     let mut btree = BTreeMap::new();
     b.iter(|| {
         for i in 1..=1_000_u64 {
@@ -39,8 +39,18 @@ fn std_btree_add_one_thousand(b: &mut Bencher) {
 }
 
 #[bench]
+fn std_btree_add_one_thousand_right_rotate(b: &mut Bencher) {
+    let mut btree = BTreeMap::new();
+    b.iter(|| {
+        for i in (1..=1_000_u64).rev() {
+            btree.insert(i, i + 1);
+        }
+    });
+}
+
+#[bench]
 fn rbtree_add_one_thousand_left_rotate(b: &mut Bencher) {
-    let mut rbtree: RedBlackTree<u64, u64> = RedBlackTree::new();
+    let mut rbtree = RedBlackTree::new();
     b.iter(|| {
         for i in 1..=1_000_u64 {
             rbtree.put(i, i + 1);
@@ -60,7 +70,7 @@ fn rbtree_add_one_thousand_right_rotate(b: &mut Bencher) {
 
 #[bench]
 fn btree_add_one_thousand_left_rotate(b: &mut Bencher) {
-    let mut btree: BalancedTree<u64, u64> = BalancedTree::new();
+    let mut btree = BalancedTree::new();
     b.iter(|| {
         for i in 1..=1000_u64 {
             btree.put(i, i + 1);
@@ -70,7 +80,7 @@ fn btree_add_one_thousand_left_rotate(b: &mut Bencher) {
 
 #[bench]
 fn btree_add_one_thousand_right_rotate(b: &mut Bencher) {
-    let mut btree: BalancedTree<u64, u64> = BalancedTree::new();
+    let mut btree = BalancedTree::new();
     b.iter(|| {
         for i in (1..=1000_u64).rev() {
             btree.put(i, i + 1);
