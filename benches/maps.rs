a86174dd2fe67ccb6d31af42aff514450b9d12a9
Horki <ivanzvonimirhorvat@gmail.com>
2020-10-01 14:22:25 +0200

    benches: added stable

--- benches/maps.rs
+++ benches/maps.rs
@@ -1,14 +1,13 @@
-#![feature(test)]
-extern crate test;
+#[macro_use]
+extern crate bencher;
 
+use bencher::Bencher;
 use std::collections::BTreeMap;
-use test::Bencher;
 use treers::bst::BST;
 use treers::btree::BalancedTree;
 use treers::rbtree::RedBlackTree;
 use treers::SedgewickMap;
 
-#[bench]
 fn bst_add_one_thousand_left_rotate(b: &mut Bencher) {
     let mut bst = BST::new();
     b.iter(|| {
@@ -18,7 +17,6 @@ fn bst_add_one_thousand_left_rotate(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn bst_add_one_thousand_right_rotate(b: &mut Bencher) {
     let mut bst = BST::new();
     b.iter(|| {
@@ -28,7 +26,6 @@ fn bst_add_one_thousand_right_rotate(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn std_btree_add_one_thousand_left_rotate(b: &mut Bencher) {
     let mut btree = BTreeMap::new();
     b.iter(|| {
@@ -38,7 +35,6 @@ fn std_btree_add_one_thousand_left_rotate(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn std_btree_add_one_thousand_right_rotate(b: &mut Bencher) {
     let mut btree = BTreeMap::new();
     b.iter(|| {
@@ -48,7 +44,6 @@ fn std_btree_add_one_thousand_right_rotate(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn rbtree_add_one_thousand_left_rotate(b: &mut Bencher) {
     let mut rbtree = RedBlackTree::new();
     b.iter(|| {
@@ -58,7 +53,6 @@ fn rbtree_add_one_thousand_left_rotate(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn rbtree_add_one_thousand_right_rotate(b: &mut Bencher) {
     let mut rbtree = RedBlackTree::new();
     b.iter(|| {
@@ -68,7 +62,6 @@ fn rbtree_add_one_thousand_right_rotate(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn btree_add_one_thousand_left_rotate(b: &mut Bencher) {
     let mut btree = BalancedTree::new();
     b.iter(|| {
@@ -78,7 +71,6 @@ fn btree_add_one_thousand_left_rotate(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn btree_add_one_thousand_right_rotate(b: &mut Bencher) {
     let mut btree = BalancedTree::new();
     b.iter(|| {
@@ -87,3 +79,27 @@ fn btree_add_one_thousand_right_rotate(b: &mut Bencher) {
         }
     });
 }
+
+benchmark_group!(
+    benches,
+    bst_add_one_thousand_left_rotate,
+    bst_add_one_thousand_right_rotate,
+    std_btree_add_one_thousand_left_rotate,
+    std_btree_add_one_thousand_right_rotate,
+    rbtree_add_one_thousand_left_rotate,
+    rbtree_add_one_thousand_right_rotate,
+    btree_add_one_thousand_left_rotate,
+    btree_add_one_thousand_right_rotate,
+);
+
+benchmark_main!(benches);
+
+// running 8 tests
+// test bst_add_one_thousand_left_rotate        ... bench:   3,440,874 ns/iter (+/- 486,622)
+// test bst_add_one_thousand_right_rotate       ... bench:   3,476,793 ns/iter (+/- 491,932)
+// test btree_add_one_thousand_left_rotate      ... bench:   1,293,973 ns/iter (+/- 289,641)
+// test btree_add_one_thousand_right_rotate     ... bench:   1,072,404 ns/iter (+/- 125,027)
+// test rbtree_add_one_thousand_left_rotate     ... bench:      92,525 ns/iter (+/- 14,757)
+// test rbtree_add_one_thousand_right_rotate    ... bench:      94,400 ns/iter (+/- 63,814)
+// test std_btree_add_one_thousand_left_rotate  ... bench:      32,799 ns/iter (+/- 12,486)
+// test std_btree_add_one_thousand_right_rotate ... bench:      32,020 ns/iter (+/- 3,540)
